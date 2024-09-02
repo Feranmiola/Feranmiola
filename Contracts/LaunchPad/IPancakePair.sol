721c2bcd1700d1a09a1665cdb4731e7b14b9c780
Ola <88761161+Feranmiola@users.noreply.github.com>
2024-09-02 14:01:13 +0100

    Delete Contracts directory

--- Contracts/LaunchPad/IPancakePair.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
-
-interface IPancakePair {
-    function balanceOf(address owner) external view returns (uint);
-    function approve(address spender, uint value) external returns (bool);
-    function transfer(address to, uint value) external returns (bool);
-    function transferFrom(address from, address to, uint value) external returns (bool);
-}
